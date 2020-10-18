7245e6d95ef3097edd84042389d246d0a1b30358
Misael gordillo <gordillo95@outlook.com>
2020-10-17 20:02:22 -0500

    Aplicación web con entity framwork instalado y catalogo de alumnos basico

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -32,7 +32,9 @@ public IActionResult Index(){
             var listaAlumnos = from n1 in nombre1
                                from n2 in nombre2
                                from a1 in apellido1
-                               select new Alumno { Nombre = $"{n1} {n2} {a1}" };
+                               select new Alumno { Nombre = $"{n1} {n2} {a1}" ,
+                                UniqueId = Guid.NewGuid().ToString()
+                               };
 
             return listaAlumnos.OrderBy((al) => al.UniqueId).Take(cantidad).ToList();
         }
